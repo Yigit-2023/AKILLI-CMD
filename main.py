ca4a2652f8b150d14453a2e8f989189bbba82b6d
Yiğit Çıtak <140903140+Yigit-2023@users.noreply.github.com>
2023-07-31 20:28:19 +0300

    Yeni komutlar ve düzenlemeler
    Değişiklikler:
    Yeni komutlar:
    Windows kısayolları.
    
    Kod satırı düzenlemesi:
    kaynak kodları daha kolay okunabilmesi için yapılan yeni düzenleme.
    
    bu noktadan sonra CMY beta çıkış yapmıştır:
    CMY_beta_001.exe

--- main.py
+++ main.py
@@ -1,15 +1,121 @@
 #Yazan Yiğit Çıtak 
+
+#Komutlar için:
 import os
+
+#Yılan oyunu için:
 import turtle
 import random
 import time
 
+#Veriler:
+
+windows_kısa = """\nWindows kısa yolları:\n
+    |F1 = Yardımı görüntüle
+    |F2 = Seçili dosyayı yeniden adlandır
+    |F3 = Bir dosyayı ya da dizini arar
+    |F4 = Dosya Gezgini’nde adres çubuğu listesini gösterir
+    |F5 = Önde çalışan pencereyi yeniler
+    |F6 = Bir penceredeki ya da masaüstündeki öğeler arasında geçiş yapar
+    |F10 = Dosya Gezgini’nde harf kısayollarını gösterir
+    |ALT + F4 = Önde çalışan programı kapatır
+    |ALT + ESC = Çalışan programları açılma sıralarına göre öne çıkarır
+    |ALT + Altı çizili harf = Önde çalışan uygulamada harf kısayollarını gösterir.
+    |ALT + Enter = Seçili dosyanın özelliklerini gösterir
+    |ALT + Space = Önde çalışan pencerenin kısayol menüsünü gösterir.
+    |ALT + Sol ok tuşu = Bir önceki sekmeye geri döner
+    |ALT + Sağ ok tuşu = Bir sonraki sekmeye geri döner
+    |ALT + Page Up = Bir sayfa boyu yukarı çıkar
+    |ALT + Page Down = Bir sayfa boyu aşağı iner
+    |ALT + TAB = Açık uygulamaları listeler
+    |CTRL + F4 = Önde çalışan uygulamayı kapatır
+    |CTRL + A = Önde çalışan sayfadaki tüm öğeleri seçer
+    |CTRL + C ya da CTRL + Insert = Seçili öğeyi kopyalar
+    |CTRL + D ya da Delete = Seçili dosyayı Geri Dönüşüm Kutusu’na gönderir
+    |CTRL + R = Seçili sayfayı yeniler
+    |CTRL + V ya da Shift + Insert = Kopyalanmış öğeyi yapıştırır
+    |CTRL + X = Seçili öğeyi keser
+    |CTRL + Z = Bir işlemi geri alır
+    |CTRL + Y = Geri alınan işlemi yeniden yapar
+    |CTRL + artı işareti (+) ya da CTRL + eksi işareti (-) = Çok sayıda dosya incelerken yakınlaştırır ya da uzaklaştırır
+    |CTRL + Fare kaydırma tuşu = Dosya Gezgini ve masaüstünde bulunan simgelerin boyutunu ayarlar
+    |CTRL + Sağ ok tuşu = İmleci bir sonraki kelimenin başına taşır
+    |CTRL + Sol ok tuşu = İmleci bir önceki kelimenin başına taşır
+    |CTRL + Aşağı ok tuşu = İmleci bir sonraki paragrafın başına taşır
+    |CTRL + Üst ok tuşu = İmleci bir önceki paragrafın başına taşır
+    |CTRL + ALT + TAB = Açık uygulama ekranını uygulama seçene kadar açık tutar
+    |CTRL + Ok tuşları + Space = Bir pencerede ya da masaüstüne birden çok öğe seçer
+    |CTRL + Shift + Ok tuşları = Bir metinde bulunan bir bloğu seçer
+    |CTRL + ESC = Başlat ekranını açar
+    |CTRL + Shift + ESC = Görev Yöneticisi’ni açar
+    |CTRL + Shift = Klavye düzenini değiştirir (Birden fazla klavye düzeni varsa)
+    |Shift + F10 = Önce çalışan uygulamanın kısayol menüsünü gösterir
+    |Shift + Delete = Seçili öğeyi kalıcı olarak siler (Geri Dönüşüm Kutusu'na yollamaz)
+    |Windows tuşu = Başlat menüsünü açar ya da kapar
+    |Windows tuşu + F1 = Yardım Alın sayfasına yönlendirir
+    |Windows tuşu + B = Gizli simgeler menüsünü seçili hâle getirir
+    |Windows tuşu + C = Charms sekmesini açar (Windows 10 hariç)
+    |Windows tuşu + D = Masaüstünü gösterir ya da gizler
+    |Windows tuşu + E = Dosya Gezgini’ni açar
+    |Windows tuşu + F = Charms sekmesinde Arama bölümünü açar (Windows 10 hariç)
+    |Windows tuşu + H = Charms sekmesinde Paylaş bölümünü açar (Windows 10 hariç)
+    |Windows tuşu + I = Charms sekmesinde Ayarlar bölümünü açar (Windows 10 hariç)
+    |Windows tuşu + K = Charms sekmesinde Cihazlar bölümünü açar (Windows 10 hariç)
+    |Windows tuşu + L = Bilgisayarı kilitler ya da kullanıcı değiştirme ekranını açar
+    |Windows tuşu + M = Tüm pencereleri küçültür
+    |Windows tuşu + P = Yansıtma seçeneklerini sıralar
+    |Windows tuşu + R = Çalıştır’ı açar
+    |Windows tuşu + S = Başlat menüsünün Arama bölümünü açar
+    |Windows tuşu + T = Görev Çubuğu’ndaki simgeler arasında gezinti sağlar
+    |Windows tuşu + U = Erişim Kolaylığı Merkezi’ni açar
+    |Windows tuşu + V = Kopyalama geçmişini açar
+    |Windows tuşu + W = Kalem’i açar
+    |Windows tuşu + X = Hızlı Bağlantı menüsünü açar
+    |Windows tuşu + Z = Bir uyygulamada tam ekran modunda kullanılabilen komutları gösterir
+    |Windows tuşu + virgül işareti (,) = Basıldığı sürece masaüstünü gösterir
+    |Windows tuşu + Pause = Sistem özelliklerini gösterir
+    |Windows tuşu + CTRL + F = Bilgisayar Bul’u açar
+    |Windows tuşu + Shift + M = Masaüstünde simge durumuna küçültülmüş pencereleri geri yükler
+    |Windows tuşu + Numara tuşları = Görev Çubuğu’nda bulunan uygulamaları sırasına göre açar
+    |Windows tuşu + Shift + Sayı tuşları =Görev Çubuğu’nda bulunan uygulamaların yeni bir penceresini açar
+    |Windows tuşu + CTRL + Sayı tuşları = Görev Çubuğu’nda bulunan uygulamaların son etkin penceresini açar
+    |Windows tuşu + ALT + Sayı tuşları = Görev Çubuğu’nda bulunan uygulamaların Atlama Listesi’ni açar
+    |Windows tuşu + CTRL + Shift + Sayı tuşları = Görev Çubuğu’nda bulunan uygulamayı yönetici olarak çalıştırır
+    |Windows tuşu + B = Son kullanılan uygulamaları listeler
+    |Windows tuşu + CTRL + TAB = Son kullanılan uygulamalar arasında gezinir
+    |Windows tuşu + Shift + TAB = Son kullanılan uygulamalar arasında ters istikamette gezinir
+    |Windows tuşu + CTRL + B = Bildirim gönderen uygulamaya geçer
+    |Windows tuşu + Yukarı ok tuşu = Önde çalışan pencereyi büyütür
+    |Windows tuşu + Aşağı ok tuşu = Önde çalışan pencereyi küçültür
+    |Windows tuşu + Sol ok tuşu = Önde çalışan pencereyi sol tarafta büyütür
+    |Windows tuşu + Sağ ok tuşu = Önde çalışan pencereyi sağ tarafta büyütür
+    |Windows tuşu + Home = Aktif olan masaüstü hariç bütün masaüstlerini küçültür
+    |Windows tuşu + Shift + Yukarı ok tuşu = Masaüstü penceresini ekranın üstüne ve altına uzatır
+    |Windows tuşu + Shift + Aşağı ok tuşu = Genişliği koruyarak etkin masaüstü pencerelerini önceki boyut ve simge durumuna küçültür
+    |Windows tuşu + Shift + Sol ok tuşu = Önde çalışan uygulamayı sol taraftaki monitöre taşır
+    |Windows tuşu + Shift + Sağ ok tuşu = Önde çalışan uygulamayı sağ taraftaki monitöre taşır
+    |Windows tuşu + Space = Giriş dili ve klavye düzenini değiştirir (Birden fazla varsa)
+    |Windows tuşu + CTRL + Space = Bir önceki seçili giriş ve klavye düzenine geçer
+    |Windows tuşu + Enter = Ekran Okuyucusu’nu açar
+    |Windows tuşu + bölme işareti (/) = IME yeniden dönüşümünü başlatır
+    |Windows tuşu + CTRL + V = Omuza dokunmaları açar
+    |Windows tuşu + CTRL + Shift + B = Bilgisayarı boş veya siyah ekrandan uyandırır
+    |Windows tuşu + artı işareti (+) = Büyüteç’i açar
+    |Windows tuşu + artı işareti (+) ya da Windows tuşu + eksi işareti (-) = Büyüteç’in yakınlaştırmasını ayarlar
+    |Windows tuşu + ESC = Büyüteç’i kapatır
+
+"""
+
 while True:
-    def ss(): #Sonraki satır
+
+    #Fonksiyonlar:
+
+    #sonrakı satır yani \n fonksiyonu
+    def ss():
         print("\n")
 
-        #Yılan Oyunu
-        #Bu oyunu ben Yazmadım. internetten buldum, ama bunun dışındaki bütün kodları ben yazdım.
+    #Yılan Oyunu fonksiyonu
+    #Bu oyunu ben Yazmadım. internetten buldum, ama bunun dışındaki bütün kodları ben yazdım.
     def oyun():
         screen = turtle.Screen()
         screen.title("YILAN OYUNU")
@@ -157,8 +263,8 @@ while True:
         turtle.Terminator
                                     #Yılan Oyunu
 
-
-    def hesapm():
+    #hesap makinesi fonksiyonu
+    def hesap_makinesi():
         xy = input("""\nToplama işlemi için "+" çıkarma için "-" çarpma için "x" yazın\n""")
 
         if xy == "+":
@@ -191,14 +297,88 @@ while True:
             print("Giriş hatalı sadece + - x bunlardan birini yazmalıydın\n")
 
 
+    #Giedi
+    giris = input("Komut Girin:\n")
 
+    #Komut Blokları:
 
-
-    giris = input("Komut Girin\n")
-
+    #komut blok 1
     if giris == "hesap makinesi" or giris == "1":
-        hesapm()
+        hesap_makinesi()
+        ss()
 
+    #komut blok 2
+    elif giris == "harf--say" or giris == "2":
+        h_say = input("Yaz: \n")
+        print(h_say," Girdisi ",len(h_say)," Harftir")
+        ss()
+
+    #komut blok 3
+    elif giris == "harf--büyüt" or giris == "3":
+        h_buyu = input("Yaz:\n")
+        print(h_buyu.upper())
+        ss()
+
+    #komut blok 4
+    elif giris == "harf--küçült" or giris == "4":
+        h_kucul = input("Yaz:\n")
+        print(h_kucul.lower())
+        ss()
+
+    #komut blok 5
+    elif giris == "harf--çevir" or giris == "5":
+        h_cevir = input("Yaz:\n")
+        print(h_cevir.swapcase())
+        ss()
+
+    #komut blok 6
+    elif giris == "metin" or giris == "6":
+        yaz = input()
+        dosya = open("metin.txt","a+")
+        dosya.write(yaz)
+        dosya.close()
+        print("İşlem başarılı")
+        ss()
+
+    #komut blok 7
+    elif giris == "kapat" or giris == "7":
+        exit()
+
+    #komut blok 8
+    elif giris == "oyun" or giris == "8":
+        print("""Oyunlar:
+            Yılan Oyunu için "1" yazın""")
+        oyun_sec = input()
+        if oyun_sec == "1":
+            print("Not: oyun kapatıldığı zaman bu program da kapanır")
+            oyun()
+        else:
+            print("oyun kodu bulunamadı")
+            ss()
+
+    #komut blok 9
+    elif giris == "kısayol" or giris == "9":
+        print(windows_kısa)
+
+
+    #Süpriz Yumurta ve imzalar:
+
+    #Süpriz Yumurta 1
+    elif giris == "kaynak kod" or giris == "2006":
+        ss()
+        print("Bu programın kaynak kodları:\nhttps://github.com/Yigit-2023/Komut-Chat")
+        ss()
+
+    #Süpriz Yumurta 2
+    elif giris == "programcı" or giris == "2023":
+        ss()
+        print("Bu Programın yazarı:\nYiğit Çıtak\nBu programın başlangıç yılı:\n2023")
+        ss()
+
+
+    #Diğer bloklar:
+
+    #Yardım blok 
     elif giris == "help":
         print("""\nKomtlar: 
             1: hesap makinesi :  hesap makinesini açar
@@ -209,7 +389,7 @@ while True:
             6: metin :  Girilen Metini kayıteder
             7: kapat :  Bu programı kapatır
             8: oyun :  Oyunları açar
-            9: kaynak kod :  Bu programın kaynak kodlarını gösterir
+            9: kısayol :  Windows kısa yolları gösterir
             10: 
             11: 
             12: 
@@ -218,58 +398,8 @@ while True:
             15: 
             """)
 
-
-
-    elif giris == "metin" or giris == "6":
-        yaz = input()
-        dosya = open("metin.txt","a+")
-        dosya.write(yaz)
-        dosya.close()
-        print("İşlem başarılı")
-        ss()
-
-    elif giris == "harf--say" or giris == "2":
-        h_say = input("Yaz: \n")
-        print(h_say," Girdisi ",len(h_say)," Harftir")
-        ss()
-
-    elif giris == "harf--büyüt" or giris == "3":
-        h_buyu = input("Yaz:\n")
-        print(h_buyu.upper())
-        ss()
-
-    elif giris == "harf--küçült" or giris == "4":
-        h_kucul = input("Yaz:\n")
-        print(h_kucul.lower())
-        ss()
-
-
-    elif giris == "kapat" or giris == "7":
-        exit()
-
-    elif giris == "harf--çevir" or giris == "5":
-        h_cevir = input("Yaz:\n")
-        print(h_cevir.swapcase())
-        ss()
-
-
-    elif giris == "oyun" or giris == "8":
-        print("""Oyunlar:
-
-            Yılan Oyunu için "1" yazın""")
-        oyun_sec = input()
-        if oyun_sec == "1":
-            print("Not: oyun kapatıldığı zaman bu program da kapanır")
-            oyun()
-        else:
-            print("oyun kodu bulunamadı")
-            ss()
-
-    elif giris == "kaynak kod" or giris == "9":
-        print("Bu programın kaynak kodları:\nhttps://github.com/Yigit-2023/Komut-Chat")
-        ss()
-
-
+    #komut bulunamadı blok
     else:   #UwU
         print("""
         >>>Komut Hatalı<<< \nKomut listesini açmak için "help" yazın""")
+        ss()
